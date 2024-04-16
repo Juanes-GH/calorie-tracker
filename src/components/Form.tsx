c5a00427cbaea75d4b6cecb0f12b1d189f2cf882
JuanesGH <jheras@2amigos.us>
2024-04-16 19:07:19 +0200

    add a state to the form fiels

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,25 @@
+import { useState, ChangeEvent } from "react"
+import type { Activity } from "../types"
 import { categories } from "../data/categories"
 
 export default function Form() {
+
+    const [ activity, setActivity ] = useState<Activity>({
+        category: 1,
+        name:'',
+        calories: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>|ChangeEvent<HTMLInputElement>) => {
+        
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+        
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ?  +e.target.value : e.target.value
+        })
+    }
+
   return (
     <form
         className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -10,11 +29,13 @@ export default function Form() {
             <select
                 className="border border-slate-300 p-2 rounded-xl w-full bg-white"
                 id="category"
+                value={activity.category}
+                onChange={handleChange}
             >
                 {categories.map(category => (
                     <option 
                         key={category.id}
-                        value={category.name}
+                        value={category.id}
                     >
                         {category.name}
                     </option>
@@ -23,12 +44,14 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
             <input 
-                id="activity"
+                id="name"
                 type="text"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. Comida, Jugo de Naranja, Ensalda, Ejercicio, Pesas, Bicicleta"
+                value={activity.name}
+                onChange={handleChange}
             />
         </div>
 
@@ -39,6 +62,8 @@ export default function Form() {
                 type="number"
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ej. 300kls o 500"
+                value={activity.calories}
+                onChange={handleChange}
             />
         </div>
 
